601ddee90a0c5b327775016176f2aea1248a6bbb
pollitoconpapass <jalbertoqz@gmail.com>
2026-07-09 23:21:54 -0500

    feat: Updated interface for doctor profiles

--- lib/screens/home_doctor_screen.dart
+++ lib/screens/home_doctor_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import '../themes/palette.dart';
+import '../widgets/bottom_navigation_bar.dart';
+import 'patients_list_screen.dart';
 
-class HomeDoctorScreen extends StatelessWidget {
+class HomeDoctorScreen extends StatefulWidget {
   const HomeDoctorScreen({super.key});
+
+  @override
+  State<HomeDoctorScreen> createState() => _HomeDoctorScreenState();
+}
+
+class _HomeDoctorScreenState extends State<HomeDoctorScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +26,24 @@ class HomeDoctorScreen extends StatelessWidget {
           'Bienvenido Dr.',
           style: TextStyle(fontSize: 24, color: deepTeal),
         ),
+      ),
+      bottomNavigationBar: AldimiBottomNavBar(
+        currentIndex: _currentIndex,
+        role: 'doctor',
+        onTap: (index) {
+          if (index == 2) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const PatientsListScreen(),
+              ),
+            );
+          } else {
+            setState(() {
+              _currentIndex = index;
+            });
+          }
+        },
       ),
     );
   }
